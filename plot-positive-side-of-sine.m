f5f3dc71cc3439fc2046c527a47239170767cafb
Manash Mandal <manashmndl@gmail.com>
2015-02-22 22:19:11 +0600

    Update plot-positive-side-of-sine.m

--- plot-positive-side-of-sine.m
+++ plot-positive-side-of-sine.m
@@ -1,2 +1,10 @@
 t = 0:1:360;
 plot (t, abs(sind(t)));
+
+% Can be done in this way too
+Tmax=2/100;
+t=linspace(0,Tmax,300); %300 points
+t = linspace (0 , Tmax, 300);
+y = 100 * sin (100 * pi * t);
+plot (t, y)
+plot (t, abs(y))
